e04f91cec1331cdce01a4349b09924b53db9ab52
mcc <ma.cc@qq.com>
2019-10-23 19:48:49 +0800

    自定义路由分发器

--- cmd/my_mux/my_mux.go
+++ cmd/my_mux/my_mux.go
@@ -1,1 +1,27 @@
-package my_mux
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type MyMux struct {
+}
+
+func (p *MyMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		sayhelloName(w, r)
+		return
+	}
+	http.NotFound(w, r)
+	return
+}
+
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	_, _ = fmt.Fprintf(w, "Hello myroute!")
+}
+
+func main() {
+	mux := &MyMux{}
+	_ = http.ListenAndServe(":9090", mux)
+}
